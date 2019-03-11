b7f8691a980b6b9f1b42151de29dacbbc2188f61
Parastoo Moghaddam <parastoomoghaddam1997@gmail.com>
2019-03-11 17:05:35 +0330

    Fix search.

--- src/containers/VisibleContactList.js
+++ src/containers/VisibleContactList.js
@@ -1,10 +1,9 @@
 import {connect} from 'react-redux'
 import ContactListPresentationalComponent from '../components/ContactsListPresentationalComponent'
-import contacts from "../reducers";
 
 const searchList = state => {
   if (state.searchedText) {
-    return state.contacts.filter(contact => contact.includes(state.searchedText))
+    return state.contacts.filter(contact => contact.name.includes(state.searchedText) || contact.number.includes(state.searchedText))
   } else {
     return state.contacts
   }
